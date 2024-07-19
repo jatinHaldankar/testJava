42fec34c866e2c5c6695172810ac790274b61450
jatinhaldankar <jatinhaldankar@EZDI-LAP0137>
2024-07-19 10:33:48 +0530

    changes in the main method

--- Test_Temp/src/com/temp/My_test.java
+++ Test_Temp/src/com/temp/My_test.java
@@ -3,7 +3,7 @@ package com.temp;
 public class My_test {
 
 	public static void main(String[] args) {
-		String name="jatin";
+		String name="jatin haldankar";
 		System.out.println("Hello World"+name);
 		System.out.println("Hello Jatin how are you?");
 	}
